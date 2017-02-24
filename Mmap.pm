0490c6dd37a27686953517c95eb5eb91bed20afe
CowboyTim <aardbeiplantje@gmail.com>
2017-02-24 23:57:42 +0100

    document the MAP_NORESERVE flag

--- Mmap.pm
+++ Mmap.pm
@@ -138,6 +138,7 @@ manually.
 
 The Mmap module exports the following constants into your namespace
     MAP_SHARED MAP_PRIVATE MAP_ANON MAP_ANONYMOUS MAP_FILE
+    MAP_NORESERVE
     MAP_HUGETLB MAP_HUGE_2MB MAP_HUGE_1GB 
     PROT_EXEC PROT_NONE PROT_READ PROT_WRITE
 
